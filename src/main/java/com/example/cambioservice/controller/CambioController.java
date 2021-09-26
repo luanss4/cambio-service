8f5ff27ab2affee6584b9fa358e1b3d19288ee39
Luan Sousa <luanss4@gmail.com>
2021-09-26 20:24:19 -0300

    configuracao de persistencia e flyway

--- src/main/java/com/example/cambioservice/controller/CambioController.java
+++ src/main/java/com/example/cambioservice/controller/CambioController.java
@@ -1,6 +1,7 @@
 package com.example.cambioservice.controller;
 
 import com.example.cambioservice.model.Cambio;
+import com.example.cambioservice.repository.CambioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+
+    @Autowired
+    private CambioRepository repository;
 
     @Autowired
     private Environment environment;
@@ -22,6 +27,14 @@ public class CambioController {
                             @PathVariable("from") String from,
                             @PathVariable("to") String to){
         var port = environment.getProperty("local.server.port");
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+        var cambio = repository.findByFromAndTo(from, to);
+        if(cambio == null){
+            throw new RuntimeException("Currency Unsupported");
+        }
+        BigDecimal conversionFactory = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactory.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnviroment(port);
+        return cambio;
     }
 }
